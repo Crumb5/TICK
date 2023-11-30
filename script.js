fc192e5d6daf5ec95aabdfe70d221a6e55ba6d39
Jared <jared@crumbs.cloud>
2023-11-30 21:42:38 +1100

    remove slider demo code

--- script.js
+++ script.js
@@ -14,33 +14,3 @@ function generateAndShowTimestamp() {
     document.getElementById('timestamp').innerText = discordTimestamp;
 }
 
-// slider
-
-document.addEventListener('DOMContentLoaded', function() {
-    const slider = document.getElementById('slider');
-    const handle = document.getElementById('handle');
-    let isDragging = false;
-
-    handle.addEventListener('mousedown', function(e) {
-        isDragging = true;
-        e.preventDefault();
-    });
-
-    document.addEventListener('mousemove', function(e) {
-        if (isDragging) {
-            let sliderRect = slider.getBoundingClientRect();
-            let newTop = e.clientY - sliderRect.top - (handle.offsetHeight / 2);
-            if (newTop < 0) newTop = 0;
-            if (newTop > slider.offsetHeight - handle.offsetHeight) newTop = slider.offsetHeight - handle.offsetHeight;
-            handle.style.top = newTop + 'px';
-        }
-    });
-
-    document.addEventListener('mouseup', function() {
-        if (isDragging) {
-            handle.style.top = '50%';
-            handle.style.transform = 'translateY(-50%)';
-            isDragging = false;
-        }
-    });
-});
