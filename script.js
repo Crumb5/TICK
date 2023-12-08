efa18aca81e5c51842984e50714c917880b9726c
Crumb5 <jared@crumbs.cloud>
2023-12-09 00:37:59 +1100

    added an actual timestamp output lol

--- script.js
+++ script.js
@@ -3,16 +3,6 @@ function generateDiscordTimestamp(date) {
     return `<t:${Math.floor(date.getTime() / 1000)}:f>`;
 }
 
-function generateAndShowTimestamp() {
-    var input = document.getElementById('datetimeInput').value;
-    var date = new Date(input);
-    if (isNaN(date.getTime())) {
-        alert('Please enter a valid date and time.');
-        return;
-    }
-    var discordTimestamp = generateDiscordTimestamp(date);
-    document.getElementById('timestamp').innerText = discordTimestamp;
-}
 
 // test
 var counter;
@@ -98,7 +88,14 @@ function updateInputFields() {
     secondInput.value = date.getSeconds();
 }
 
-
+function generateAndShowTimestamp() {
+    if (isNaN(date.getTime())) {
+        alert('Please enter a valid date and time.');
+        return;
+    }
+    var discordTimestamp = generateDiscordTimestamp(date);
+    document.getElementById('timestamp').innerText = discordTimestamp;
+}
 
 
 
