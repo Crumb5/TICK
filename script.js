18e6bf88f5f006f1d0afa20a6ca9c91592e88304
Crumb5 <jared@crumbs.cloud>
2023-12-09 00:12:39 +1100

    a commit

--- script.js
+++ script.js
@@ -15,36 +15,26 @@ function generateAndShowTimestamp() {
 }
 
 // test
-
-var slider = document.getElementById("mySlider");
-var sliderValue = document.getElementById("sliderValue");
-var resultValue = document.getElementById("resultValue");
 var counter;
 var date = new Date();
 
-function updateTimeDisplay() {
-    var date = new Date();
-    resultValue.innerHTML = date.toLocaleString();
-}
+updateInputFields()
 
-function populateDateInputs(date) {
-    document.getElementById('yearInput').value = date.getFullYear();
-    document.getElementById('monthInput').value = date.getMonth() + 1;
-    document.getElementById('dayInput').value = date.getDate();
-    document.getElementById('hourInput').value = date.getHours();
-    document.getElementById('minuteInput').value = date.getMinutes();
-    document.getElementById('secondInput').value = date.getSeconds();
-}
+var inputElements = document.querySelectorAll('.slider');
 
-populateDateInputs(new Date());
+inputElements.forEach(function(slug){
+    slug.oninput = function(){
+        console.log('Log:', (this.id).replace('Slider', ''));
+        slugSlide(this, (this.id).replace('Slider', ''));
+    };
+    slug.onchange = function(slug){
+        this.value = 0;
+        console.log("On Changed!");
+        clearInterval(counter);
+    };
+});
 
-updateTimeDisplay();
-
-resultValue.innerHTML = date.toLocaleString();
-
-slider.oninput = function() {
-
-    sliderValue.innerHTML = this.value;
+function slugSlide(slugSliderElement, slugDatePart){
 
     if (counter !== null) {
         clearInterval(counter);
@@ -52,31 +42,61 @@ slider.oninput = function() {
 
     minRate = 700
     maxRate = 50
-    currRate = (1 - (Math.abs(this.value) / slider.max)) * (minRate - maxRate) + maxRate;
+    currRate = (1 - (Math.abs(slugSliderElement.value) / slugSliderElement.max)) * (minRate - maxRate) + maxRate;
 
     counter = setInterval(() => {
-        if (this.value > 0) {
-            date.setMinutes(date.getMinutes() + 1);
-        } else if (this.value < 0) {
-            date.setMinutes(date.getMinutes() - 1);
+        if (slugSliderElement.value > 0) {
+            slugDisplayUpdate(slugDatePart,1)
+        } else if (slugSliderElement.value < 0) {
+            slugDisplayUpdate(slugDatePart,-1)
         } else {
             clearInterval(counter);
         }
-        resultValue.innerHTML = date.toLocaleString();
-        console.log("Current Rate:", currRate);
+        updateInputFields()
     }, currRate);
-};
-
-slider.onchange = function() {
-    this.value = 0;
-    console.log("On Changed!");
-    clearInterval(counter);
-    sliderValue.innerHTML = this.value;
-};
+}
 
 
+function slugDisplayUpdate(slugDatePart, incrementBy) {
+    switch (slugDatePart) {
+        case 'year':
+            date.setFullYear(date.getFullYear() + incrementBy);
+            break;
+        case 'month':
+            date.setMonth(date.getMonth() + incrementBy);
+            break;
+        case 'day':
+            date.setDate(date.getDate() + incrementBy);
+            break;
+        case 'hour':
+            date.setHours(date.getHours() + incrementBy);
+            break;
+        case 'minute':
+            date.setMinutes(date.getMinutes() + incrementBy);
+            break;
+        case 'second':
+            date.setSeconds(date.getSeconds() + incrementBy);
+            break;
+        default:
+            console.log('Invalid date part:', slugDatePart);
+    }
+}
 
+function updateInputFields() {
+    var yearInput = document.getElementById("yearInput");
+    var monthInput = document.getElementById("monthInput");
+    var dayInput = document.getElementById("dayInput");
+    var hourInput = document.getElementById("hourInput");
+    var minuteInput = document.getElementById("minuteInput");
+    var secondInput = document.getElementById("secondInput");
 
+    yearInput.value = date.getFullYear();
+    monthInput.value = date.getMonth() + 1;
+    dayInput.value = date.getDate();
+    hourInput.value = date.getHours();
+    minuteInput.value = date.getMinutes();
+    secondInput.value = date.getSeconds();
+}
 
 
 
